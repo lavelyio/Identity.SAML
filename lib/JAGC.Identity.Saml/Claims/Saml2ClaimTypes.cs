4403d6efda05a5bd4ad55055486ca4c1593ad9de
Josh Lavely <Josh@lavely.io>
2022-04-29 11:17:56 -0400

    Updated for IL4

--- lib/JAGC.Identity.Saml/Claims/Saml2ClaimTypes.cs
+++ lib/JAGC.Identity.Saml/Claims/Saml2ClaimTypes.cs
@@ -3,8 +3,8 @@
     public static class Saml2ClaimTypes
     {
         public const string NameId = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name";
-        public const string NameIdFormat = "urn:oasis:names:tc:SAML:2.0:nameid-­format:persistent";
-        public const string Email = "urn:oasis:names:tc:SAML:2.0:attrname-­format:basic";
+        public const string NameIdFormat = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress";
+        public const string Email = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress";
         public const string GivenName = "https://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname";
         public const string Surname = "https://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname";
         public const string SessionIndex = "http://schemas.xmlsoap.org/ws/2014/02/identity/claims/saml2sessionindex";
